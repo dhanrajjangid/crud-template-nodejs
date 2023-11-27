5b5b5f80d51f9c19a26384e8c7c7eae182a0e9e1
dhanrajjangid22 <dhanraj.jangid@ignatiuz.com>
2023-11-27 13:15:12 +0530

    first commit

---
+++ src/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+require('dotenv').config(); // Load environment variables from .env
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Routes
+app.use('/users', userRoutes); // Use '/users' for user-related routes
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
